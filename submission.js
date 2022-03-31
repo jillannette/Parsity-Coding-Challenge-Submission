66aae4cf6896087a895f064540de04ebe8821487
jillannette <jillarnold2913@gmail.com>
2022-03-31 14:55:47 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -39,5 +39,39 @@ const removeParenth = function(str) {
 };
 
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+  string = string.toLowerCase();
+  let points = {
+  a: 1,
+  e: 1,
+  i: 1,
+  o: 1,
+  u: 1,
+  l: 1,
+  n: 1,
+  r: 1,
+  s: 1,
+  t: 1,
+  d: 2,
+  g: 2,
+  b: 3,
+  c: 3,
+  m: 3,
+  p: 3,
+  f: 4,
+  h: 4,
+  v: 4,
+  w: 4,
+  y: 4,
+  k: 5,
+  j: 8,
+  x: 8,
+  q: 10,
+  z: 10,
+},
+sum = 0; 
+
+  for (i = 0; i < string.length; i++) {
+    sum += points[string[i]] || 0;
+  }
+  return sum;
 };
