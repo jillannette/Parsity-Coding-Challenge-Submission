fd1a15750278b20aae6b4dd2017ca85f3dd5e9f0
jillannette <jillarnold2913@gmail.com>
2022-03-31 14:32:56 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -17,7 +17,18 @@ const isPalindrome = function(str) {
 };
 
 const largestPair = function(array) {
-  // your code here - don't forget to return a number!
+  var arr = array;
+  var x = 0;
+  var y = 0;
+  var p = Number.MIN_SAFE_INTEGER;
+  for (var i = 0; i < arr.length; i++) {
+    x = arr[i];
+    y = arr[i + 1];
+    if (x * y > p) {
+      p = x * y;
+    };
+  };
+  return p;
 };
 
 const removeParenth = function(str) {
